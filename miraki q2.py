229b7f8275ad74855a9e03703431fda381d99685
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2021-11-16 19:39:53 +0530

    Add files via upload

---
+++ miraki q2.py
@@ -0,0 +1,5 @@
+number=int(input("please enter the number:"))
+if number>=44*30:
+    print(number,"greater than or equal")
+else:
+    print("lesser")
---
+++ miraki q4.py
@@ -0,0 +1,5 @@
+varx=int(input("please enter number:"))
+if varx%2==0:
+    print(varx,"is divisible")
+else:
+    print(varx,"is not divisible")
